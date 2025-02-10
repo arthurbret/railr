f0e3aeff6050571b4e4149ce7c9c140bc887e8f3
Lucas <gosgnach24lucas@gmail.com>
2025-02-10 12:52:38 +0100

    feat: Change train search design

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -28,7 +28,7 @@ function KeyboardAwareWrapper({ children }: { children: React.ReactNode }) {
 const Footer: React.FC = () => {
   return (
     <div>
-      <footer className="flex justify-between border border-neutral-200 items-center max-w-fit p-4 shadow-lg bg-localblue/50 fixed bottom-0 inset-x-0 mx-auto transform rounded-lg m-4 z-50 backdrop-blur-[7px] gap-[15px] shadow">
+      <footer className="flex justify-between border border-neutral-200 items-center max-w-fit fixed bottom-0 inset-x-0 mx-auto transform rounded-lg m-4 z-50 backdrop-blur-[7px] gap-[15px] shadow">
         <KeyboardAwareWrapper>
           <div className="flex items-center gap-4 border-0">
             <Combobox />
