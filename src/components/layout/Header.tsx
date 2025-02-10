dcc9a7d6e1abfd2e6b42dbabf183034696764635
Lucas <gosgnach24lucas@gmail.com>
2025-02-10 13:02:57 +0100

    design: fix design app

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,16 +4,18 @@ import Image from 'next/image';
 import React from 'react';
 
 const Header: React.FC = () => {
-  return (
-    <div>
-      <header className="flex justify-between items-center p-4 bg-localblue/50 fixed rounded-lg m-4 z-50 backdrop-blur-[7px] gap-[15px] shadow-lg">
-        <Image src="/logo_traintracker.jpg" alt="Logo" width={40} height={40} className='rounded' />
-        <h1 className="text-xl font-bold">TrainTracker</h1>
-        <div className="flex items-center gap-4"></div>
-      </header>
-      <div className="h-[90px]"></div>
-    </div>
-  );
+    return (
+        <div>
+            <header
+                className="flex justify-between items-center p-4 bg-localblue/50 fixed rounded-lg m-4 z-50 backdrop-blur-[7px] gap-[15px] shadow-lg">
+                <Image src="/logo_traintracker.jpg" alt="Logo" width={40} height={40}
+                       className="rounded-full shadow-md border border-white/30"/>
+                <h1 className="text-xl font-bold">TrainTracker</h1>
+                <div className="flex items-center gap-4"></div>
+            </header>
+            <div className="h-[90px]"></div>
+        </div>
+    );
 };
 
 export default Header;
